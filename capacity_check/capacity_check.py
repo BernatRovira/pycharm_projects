f3dd6e0d5868536a97334bf79e428f68a2bad45e
bernat <bernatrovira@gmail.com>
2018-11-30 19:19:18 +0100

    custom probability for w added
    first ideas for w updating with vector products

--- capacity_check/capacity_check.py
+++ capacity_check/capacity_check.py
@@ -1,30 +1,41 @@
 # ............... IMPORTS
 from scipy.sparse import csr_matrix   # import compressed sparse row
 from random import sample             # import sample (rnd from a list, no repetition)
+from random import randrange          # generate a random number between two integers
 from random import shuffle            # random reorganization of an array
 #from numpy.ndarray import resize           # reshape an array into another shape
 import numpy as np
 # ............... File headers
-from definitions import N, L, f
+from definitions import N, L, f, q_10, q_01
 
 # ............... Generate patterns
 pattern = np.zeros((L, N))  # L rows and N columns matrix to store the patterns
 for i in range(L):   # for each of the L memories   TODO eliminate the for loop somehow
     pattern[i][sample(range(int(N)), int(N*f))] = 1  # activate N*f random neurons
-sparse_pattern = csr_matrix(pattern)  # transform the patterns matrix into a sparse matrix
+# sparse_pattern = csr_matrix(pattern)  # transform the patterns matrix into a sparse matrix
 
 # ............... Generate connectivity matrix
-#w = np.zeros((N, N)) # connectivity matrix initialized at zero values
-w = np.array([0]*int(N*N*(1-f))+[1]*int(N*N*f)) # create a vector with N*N(1-f) zeros and N*N*f ones
-shuffle(w)  # shuffle it to make it random
-w.resize((N, N))    # resize it to N*N shape
+w = np.zeros((N, N)) # connectivity matrix initialized at zero values
+# w = np.array([0]*int(N*N*(1-f))+[1]*int(N*N*f)) # create a vector with N*N(1-f) zeros and N*N*f ones
+# shuffle(w)  # shuffle it to make it random
+# w.resize((N, N))    # resize it to N*N shape
 
-#for i in range(L):   # for each of the L memories
-#    potmat = np.outer(pattern[i][:], pattern[i][:])  # create potentiation matrix from pattern i vector product
-#    depmat_10 = np.outer(pattern[i][:], 1-pattern[i][:])  # create depression matrix 10 from i and 1-i
-#    depmat_01 = np.outer(1-pattern[i][:], pattern[i][:])  # create depression matrix 01 from i and 1-i
+for i in range (L):  # range(L):   # for each of the L memories
+    potmat = np.outer(pattern[i][:], pattern[i][:])  # create potentiation matrix from pattern i vector product (qp=1)
 
-# penis
+    depmat10 = np.outer(pattern[i][:], 1-pattern[i][:])  # create depression matrix from pattern i reversed vector
+    depmat10[np.nonzero(depmat10)] *= np.random.rand(f*(1-f)*N**2)  # multiply non zeros by random number
+    depmat10[depmat10>=q_10] = 0                         # erase all randoms higher than q_10
+    depmat10[0<depmat10] = 1                      # turn to events all randoms lower than q_10
 
+    depmat01 = np.outer(1-pattern[i][:], pattern[i][:])  # create depression matrix from pattern i reversed vector
+    depmat01[np.nonzero(depmat01)] *= np.random.rand(f*(1-f)*N**2)  # multiply non zeros by random number
+    depmat01[depmat01>q_01] = 0                         # erase all randoms higher than q_10
+    depmat01[0<depmat01<=q_01] = 1                      # turn to events all randoms lower than q_10
 
+    potdep = potmat-depmat10-depmat01
+w += potdep
+np.clip(w, 0, 1)
+
+print w
 
--- capacity_check/definitions.py
+++ capacity_check/definitions.py
@@ -1,15 +1,14 @@
 # ............... Model parameters 
 import numpy as np
-N = np.float64(1e1)      # Neuron number
-L = 10         # number of patterns to relax con. matrix
-f = 0.5  # coding level
-gama = 1e3   # sigmoid constant
-t_E = 1.      # Excitatory time constant
-t_I = 0.5     # Inhibitory time constant
+N = np.float64(1e3)      # Neuron number
+L = 200        # number of patterns to relax con. matrix
+f = 0.015      # coding level
+q_01 = 0.0075  # depression probability 01
+q_10 = 0.0075  # depression probability 10075gama = 1e3   # sigmoid constant075t_E = 1.      # Excitatory time constan075075t_I = 0.5     # Inhibitory time constant
 t_W = 1.0e2    # Weight time constant
-I_E = 0.      # Excitatory constant input
-I_I = 0.      # Inhibitory constant input
-theta = 0.5   # Treshold
+I_E = 0.       # Excitatory constant input
+I_I = 0.       # Inhibitory constant input
+theta = 0.5    # Treshold
 
 # ............... Simulation parameters 
 dt = 0.1      # Time step
