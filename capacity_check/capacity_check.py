bf90d58e14d49b34636e601db916eae8c7e8c81c
bernat <bernatrovira@gmail.com>
2018-12-04 20:04:13 +0100

    added crude testing method, still to develop

--- capacity_check/capacity_check.py
+++ capacity_check/capacity_check.py
@@ -1,41 +1,85 @@
 # ............... IMPORTS
 from scipy.sparse import csr_matrix   # import compressed sparse row
 from random import sample             # import sample (rnd from a list, no repetition)
-from random import randrange          # generate a random number between two integers
 from random import shuffle            # random reorganization of an array
-#from numpy.ndarray import resize           # reshape an array into another shape
 import numpy as np
+import matplotlib.pyplot as plt
+from definitions import CodeTimer
+
 # ............... File headers
-from definitions import N, L, f, q_10, q_01
+from definitions import N, L, f, q_, q_10, q_01, g, gp
+
+print N, L, q_, g, int(N*f)
+
+# print L, int(N*(1.-f)), int(N*f), np.ceil(N*(1-f)), 16*984
+# raw_input('debugger')
+
 
 # ............... Generate patterns
 pattern = np.zeros((L, N))  # L rows and N columns matrix to store the patterns
-for i in range(L):   # for each of the L memories   TODO eliminate the for loop somehow
+for i in range(int(L)):   # for each of the L memories   TODO eliminate the for loop somehow
     pattern[i][sample(range(int(N)), int(N*f))] = 1  # activate N*f random neurons
-# sparse_pattern = csr_matrix(pattern)  # transform the patterns matrix into a sparse matrix
+sparse_pattern = csr_matrix(pattern)  # transform the patterns matrix into a sparse matrix
 
 # ............... Generate connectivity matrix
-w = np.zeros((N, N)) # connectivity matrix initialized at zero values
-# w = np.array([0]*int(N*N*(1-f))+[1]*int(N*N*f)) # create a vector with N*N(1-f) zeros and N*N*f ones
+n_ones_depmat = int(N*f)*np.ceil(N*(1.-f))  # number of ones in the depression matrix
+w_evo = np.zeros(L)   # evolution of weight matrix started at zero also
+w_steady = 1/(1+(1-f)*q_/(f-1/N))  # expected avg value of steady w, accounting for small N
+w = np.zeros((N, N))  # connectivity matrix initialized at zero values
+# w = np.array([0]*int(N*N*(1-w_steady))+[1]*np.round(N*N*w_steady))  # vector with N^2*w_steady ones
 # shuffle(w)  # shuffle it to make it random
 # w.resize((N, N))    # resize it to N*N shape
 
-for i in range (L):  # range(L):   # for each of the L memories
-    potmat = np.outer(pattern[i][:], pattern[i][:])  # create potentiation matrix from pattern i vector product (qp=1)
 
+for i in range(int(L)):  # range(L):   # for each of the L memories
+    if (i % 100) == 0:
+        print i
+
+    # with CodeTimer('potmat'):
+    potmat = np.outer(pattern[i][:], pattern[i][:])  # create pot matrix from pattern i vector product (qp=1)
+
+    # with CodeTimer('depmat10'):
     depmat10 = np.outer(pattern[i][:], 1-pattern[i][:])  # create depression matrix from pattern i reversed vector
-    depmat10[np.nonzero(depmat10)] *= np.random.rand(f*(1-f)*N**2)  # multiply non zeros by random number
-    depmat10[depmat10>=q_10] = 0                         # erase all randoms higher than q_10
-    depmat10[0<depmat10] = 1                      # turn to events all randoms lower than q_10
+    # rndvec = np.random.rand(n_ones_depmat)  # generate as many rnd as ones in the matrix
+    # rndvec[np.where(rndvec < q_10)] = 1     # set the rnd below prob to one (they will modify)
+    # np.floor(rndvec, rndvec)                        # and the rest to zero (they will not modify)
+    # depmat10[np.where(depmat10 > 0)] *= rndvec  # multiply non zeros by random number
+    rndvec = (np.random.rand(n_ones_depmat) < q_10)
+    depmat10[depmat10 > 0] = depmat10[depmat10 > 0]*rndvec
 
+    # with CodeTimer('depmat01'):
     depmat01 = np.outer(1-pattern[i][:], pattern[i][:])  # create depression matrix from pattern i reversed vector
-    depmat01[np.nonzero(depmat01)] *= np.random.rand(f*(1-f)*N**2)  # multiply non zeros by random number
-    depmat01[depmat01>q_01] = 0                         # erase all randoms higher than q_10
-    depmat01[0<depmat01<=q_01] = 1                      # turn to events all randoms lower than q_10
+    # rndvec[np.where(rndvec < q_01)] = 1     # set the rnd below prob to one (they will modify)
+    # np.floor(rndvec, rndvec)                        # and the rest to zero (they will not modify)
+    # depmat01[np.where(depmat01 > 0)] *= rndvec  # multiply non zeros by random number
+    rndvec2 = (np.random.rand(n_ones_depmat) < q_01)
+    depmat01[depmat01 > 0] = depmat01[depmat01 > 0]*rndvec2
 
-    potdep = potmat-depmat10-depmat01
-w += potdep
-np.clip(w, 0, 1)
+    # print i, depmat10.sum(), depmat01.sum()
+    # with CodeTimer('weight updating'):
+    w = np.clip(w+potmat-depmat10-depmat01, 0, 1)  # apply synapse modifications and clip to boundaries
+    # w[potmat > 0] += potmat[potmat > 0]   # alternative method, slower
+    # w[depmat10 > 0] -= depmat10[depmat10 > 0]
+    # w[depmat01 > 0] -= depmat01[depmat01 > 0]
+    # w[w != 0] = np.clip(w[w != 0], 0, 1)
+    # w = w+potmat-depmat10-depmat01        # another one, also slower
+    # w[w>1] = 1
+    # w[w<0] = 0
 
-print w
+    # with CodeTimer('weight counting'):
+    # w_evo[i] = np.count_nonzero(w)                 # count nonzero elements to record w evolution
+sparse_w = csr_matrix(w)
 
+# plt.plot(w_evo)
+# plt.axhline(y=N*N*w_steady, color='r', linestyle='-')
+# plt.show()
+
+# ............... Stability testing
+n_test = 150
+test_result = np.zeros(n_test)
+for i in range(int(L)-n_test, int(L)):
+    state = w.dot(pattern[i][:].T)
+    mask_state = state > N*f*gp
+    if np.all(mask_state == pattern[i][:]):
+        test_result[i-n_test] += 1
+print test_result
--- capacity_check/definitions.py
+++ capacity_check/definitions.py
@@ -1,16 +1,36 @@
-# ............... Model parameters 
 import numpy as np
-N = np.float64(1e3)      # Neuron number
-L = 200        # number of patterns to relax con. matrix
-f = 0.015      # coding level
-q_01 = 0.0075  # depression probability 01
-q_10 = 0.0075  # depression probability 10075gama = 1e3   # sigmoid constant075t_E = 1.      # Excitatory time constan075075t_I = 0.5     # Inhibitory time constant
-t_W = 1.0e2    # Weight time constant
-I_E = 0.       # Excitatory constant input
-I_I = 0.       # Inhibitory constant input
-theta = 0.5    # Treshold
+# ............... Model parameters
+N = np.float64(3e2)      # Neuron number
+beta = 2.44     # parameter for coding level
+gama = 100      # sigmoid steepness
+theta = 0.0    # Treshold
+f = beta*np.log(N)/N     # coding level
+qp = 1 # potentiation probability
+q_ = 2.57*f/(1-f)   # depression probability
+q_01 = q_/2  # depression probability 01
+q_10 = q_/2  # depression probability 10
+L = np.round(np.log(1e-5)/np.log(1-qp*f**2-f*(1-f)*q_))  # Lsteady eq 2.20 thesis draft
+g = 1/(1+(1-f)*q_/f)
+gp = g+(1-g)*np.exp(-0.14/g)
+w_EI = gp       # inh to exc synaptic weight
+t_E = 1.      # Excitatory time constant
+t_I = 0.5     # Inhibitory time constant
 
 # ............... Simulation parameters 
 dt = 0.1      # Time step
 rlzt = 10     # Realizations per iteration
 test_n = 150  # Number of patterns to be tested
+
+
+import time
+
+class CodeTimer:
+    def __init__(self, name=None):
+        self.name = " '"  + name + "'" if name else ''
+
+    def __enter__(self):
+        self.start = time.clock()
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.took = (time.clock() - self.start) * 1000.0
+        print('Code block' + self.name + ' took: ' + str(self.took) + ' ms')
